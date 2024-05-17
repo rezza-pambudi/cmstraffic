ae02fe5c76c19d6c2ed4996b501251177237fe88
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2024-05-17 12:31:56 +0700

    update resource

--- app/Filament/Resources/DocumentResource.php
+++ app/Filament/Resources/DocumentResource.php
@@ -2,16 +2,19 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\DocumentResource\Pages;
-use App\Filament\Resources\DocumentResource\RelationManagers;
-use App\Models\Document;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use App\Models\Document;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\MarkdownEditor;
+use App\Filament\Resources\DocumentResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\DocumentResource\RelationManagers;
 
 class DocumentResource extends Resource
 {
@@ -25,7 +28,26 @@ class DocumentResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('id')->unique(ignorable: fn ($record) => $record)->hidden()->afterStateUpdated(function (?string $state, ?string $old) {
+                    $state()->update();
+                }),
+                TextInput::make('nama')->required(),
+                Select::make('kanal')->searchable()->options([
+                    'Detikcom' => 'Detikcom',
+                    'CNN Indonesia' => 'CNN Indonesia',
+                    'CNBC Indonesia' => 'CNBC Indonesia',
+                    'Haibunda & Insertlive' => 'Haibunda & Insertlive',
+                    'All Kanal' => 'All Kanal',
+                ])->preload(),
+                Select::make('web')->searchable()->options([
+                    'Detikcom' => 'Detikcom',
+                    'CNN Indonesia' => 'CNN Indonesia',
+                    'CNBC Indonesia' => 'CNBC Indonesia',
+                    'Haibunda & Insertlive' => 'Haibunda & Insertlive',
+                    'All Kanal' => 'All Kanal',
+                ])->preload(),
+                MarkdownEditor::make('deskripsi')->required()->columnSpanFull()->label('Deskripsi'),
+                MarkdownEditor::make('link')->required()->columnSpanFull()->label('Link')
             ]);
     }
 
